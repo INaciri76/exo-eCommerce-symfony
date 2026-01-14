5dc1f063b9d77be1b853d816ebc4d0fb418b1dbd
Inene <inene.naciri00@gmail.com>
2026-01-14 16:41:49 +0100

    modif Route + ajout fonction orders

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,13 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
+ #[Route('/', name: 'index')]
 final class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
+   
     public function index(): Response
     {
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
         ]);
+    }
+
+    #[Route('/orders', name: 'orders')]
+    public function orders(): Response
+    {
+        return $this->render('profile/orders.html.twig');
     }
 }
